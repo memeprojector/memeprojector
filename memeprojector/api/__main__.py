a190279d6447ba4e6b0be6ca6fc7c2d6764382f4
Jesper Larsson <y.JesperLarsson@gmail.com>
2023-03-04 00:11:27 +0100

    feat: Add skeleton structure for API

---
+++ memeprojector/api/__main__.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    image: str
+    epsg: int
+
+@app.get("/")
+def index():
+    return {"text": "Welcome to our EPSG projection application!"}
+
+@app.post("/project")
+def project(item: Item):
+    return {
+        "image": item.image,
+        "epsg": item.epsg
+        }
+
+@app.get("/list_projections")
+def list_projections():
+    return [
+        "EPSG:1234",
+        "EPSG:5678",
+        "EPSG:9123",
+    ]
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
