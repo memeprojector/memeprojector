0cad93b828b44c3af4e9d54398c05dc33b46b041
Elie-Alban LESCOUT <elie-alban.lescout@eurosense.com>
2023-03-26 03:01:50 +0200

    functional projector

--- memeprojector/projector/projector.py
+++ memeprojector/projector/projector.py
@@ -2,49 +2,37 @@ import rasterio
 import numpy as np
 from PIL import Image
 from pathlib import Path
+import matplotlib.pyplot as plt 
 from tempfile import TemporaryDirectory
 from rasterio.transform import from_gcps
 from rasterio.control import GroundControlPoint
-from rasterio.warp import (
-    calculate_default_transform,
-    reproject, Resampling
-)
+from rasterio.warp import calculate_default_transform, reproject
 
 def project(img: np.array, epsg: int) -> np.array:
-    print(img.shape)
+    # Input
+    in_crs = '+proj=latlong'
+    gcps = get_GCPs(img.shape)
+    in_transform = from_gcps( gcps )
+
+    # Output CRS, transform and array
     out_crs = rasterio.crs.CRS.from_epsg(epsg)
-    transf = from_gcps( get_GCPs(img.shape) )
-    
-    with TemporaryDirectory() as tmp_path:
-        tmp_path = Path(tmp_path)
-        with rasterio.open(
-            tmp_path / 'tmp.tif',
-            'w',
-            driver='GTiff',
-            height=img.shape[0],
-            width=img.shape[1],
-            count=img.shape[2],
-            dtype=img.dtype,
-            crs='+proj=latlong',
-            transform=transf,
-        ) as swap:
-            for i in range(img.shape[2]):
-                swap.write(img[:,:,i], i+1)
-            (  out_transform,
-               out_width,
-               out_height    ) = calculate_default_transform(
-                                    swap.crs, out_crs,
-                                    swap.width, swap.height,
-                                    *swap.bounds)
-            
-            out_img, out_transf = reproject(
-                        img[:,:,0],
-                        src_transform=swap.transform,
-                        src_crs=swap.crs,
-                        #dst_transform=out_transform,
-                        dst_crs=out_crs,
-                        )
-    return out_img
+    out_transform, out_width, out_height = calculate_default_transform(
+        in_crs, out_crs, img.shape[0], img.shape[1], gcps=gcps)
+
+    out_imgs = []
+    # Reproject image for each band
+    for band in range(img.shape[-1]):
+        out_img = np.zeros((out_width, out_height))
+        out_img, out_transform = reproject(
+            img[:,:,band],
+            out_img,
+            src_transform=in_transform,
+            src_crs=in_crs,
+            dst_transform=out_transform,
+            dst_crs=out_crs,
+        )
+        out_imgs.append(out_img)
+    return np.stack(out_imgs, axis=2)/255
 
 def get_GCPs(img_shape: tuple) -> list:
     row_max, col_max = img_shape[0], img_shape[1]
@@ -55,7 +43,7 @@ def get_GCPs(img_shape: tuple) -> list:
             row=0,
             col=0,
             x=xmin,
-            y=ymin,           
+            y=ymax,           
         ),
         GroundControlPoint(
             row=row_max,
@@ -83,12 +71,13 @@ def get_GCPs(img_shape: tuple) -> list:
         ),
     ]
 
-if __name__ == '__main__':
-    root = Path(__file__).parents[2]
-    with Image.open(root/"test/spiderman-meme.png") as im:
-        out_im = project(
-            np.asarray(im),
-            31467
-        )
-    im = Image.fromarray(out_im)
-    im.save(root/"projected.png")
+# if __name__ == '__main__':
+#     root = Path(__file__).parents[2]
+#     with Image.open(root/"test/spiderman-meme.png") as im:
+#         out_im = project(
+#             np.asarray(im),
+#             2154
+#         )
+#         print('in_size:', np.asarray(im).shape)
+#     print('out_size:', out_im.shape)
+#     plt.imsave(root/'test'/'projected.png', out_im)
